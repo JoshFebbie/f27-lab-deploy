e9da8227f654500a6eb47f4940171a3b1c5b3865
Josh Febbie <joshdfebbie@gmail.com>
2022-08-31 16:37:25 -0700

    second attempt

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+// Endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"));
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/style.css"));
+});
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/client.js"));
+});
+
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
+
